0a534c0d47ebb75020a61db6954001afbf8fb686
Kevin Porter <kevporter20@yahoo.co.uk>
2025-05-13 13:18:56 +0100

    Switch to DataGrid component for loading, paging and sorting support

--- GameLibrary/gamelibrary-ui/src/pages/GenreListPage.tsx
+++ GameLibrary/gamelibrary-ui/src/pages/GenreListPage.tsx
@@ -1,53 +1,48 @@
-import { TableContainer, Table, TableHead, TableBody, TableRow, TableCell, Paper } from "@mui/material";
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import { useNotifications } from "@toolpad/core";
 import { useEffect, useState } from "react";
 import { Genre } from "../dto/genre";
 
 function GenreListPage() {
     const [genres, setGenres] = useState<Genre[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
 
     const notifications = useNotifications();
+
+    const columns : GridColDef[] = [
+        { field: "name", headerName: "Name" }
+    ]
     
     useEffect(() => {
         loadGenres();
     }, []);
 
     const loadGenres = async () => {
+        setIsLoading(true);
+
         await fetch('http://localhost:5261/api/Genre', {
             method: 'GET'
         })
         .then((resp) => resp.json())        
-        .then((data) => setGenres(data))
+        .then((data) => {
+            setGenres(data);
+            setIsLoading(false);
+        })
         .catch((err) => {
             notifications.show(`An error has occurred: ${err.message}`, {
                 autoHideDuration: 2000,
                 severity: "error"
             })
+            setIsLoading(false);
         });
     };
 
-    return (
-        <>
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }}>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Name</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {genres.map((row) => (
-                            <TableRow 
-                                key={row.id}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                                    <TableCell component="th" scope="row">
-                                        {row.name}
-                                    </TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+    return (<>
+        <DataGrid
+            rows={genres}
+            columns={columns}
+            loading={isLoading}            
+        />
         </>
     )
 }
